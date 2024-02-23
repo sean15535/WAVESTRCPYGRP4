a7c6e3a6cd2def1557c97a734c310cf3444283c5
sean15535 <sean15535@gmail.com>
2024-02-23 19:08:14 +0100

    adding greeting and variables

--- Bank app.py
+++ Bank app.py
@@ -1,1 +1,9 @@
-print("Welcome to Wema Bank Plc")
+#Oluwaseun as the gropu leader started with the greetings messagenand the variables
+x =[0,1,2,3,4,5,6,7,8,9]
+print("Welcome to Wema Bank Plc. \nHow maywe help you today?")
+print("1. Open account\n2. Transfer\n3. Airtime\n4. Internet\n5. Balance\n6. Bills & Utitilies")
+response = str(input()).lower()
+
+
+
+
